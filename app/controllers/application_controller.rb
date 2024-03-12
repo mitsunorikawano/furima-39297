b12ec42720119ac91f77787c1de6426e16d1731c
mitsunorikawano <mitsunori94@yahoo.co.jp>
2024-03-12 22:48:35 +0900

    ログイン機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,16 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  # before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def index
+  protected
+ 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :birth_date, :first_name, :last_name, :first_name_kana, :last_name_kana])
   end
-
-  private
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == 'admin' && password == '2222'
-      #username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
-
-      # username == 'admin' && password == '2222'
     end
   end
-
-
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "は有効な形式で入力してください" }
+  validates :password, presence: true, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/, message: "は半角英数字混合で入力してください" }
+  validates :password, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, confirmation: true
+
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :name, presence: true, format: { with: /\A[\p{Han}\p{Hiragana}\p{Katakana}]+\z/, message: "は全角文字で入力してください" }
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: "は全角カタカナで入力してください" }
+  validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: "は全角カタカナで入力してください" }
+  validates :birth_date, presence: true
 end
